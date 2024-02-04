868de8ab15e68b085f9be4ade2efbca3b7d681ab
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-02-04 17:14:18 +0500

    cookies parser done

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import User from "../models/User.js"
 import { hash,compare } from 'bcrypt'
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (req:Request,res:Response,next:NextFunction) => {
     try {
@@ -26,6 +27,22 @@ export const userSignup = async (req:Request,res:Response,next:NextFunction) => 
     const hashedPassword = await hash(password,10);
         const user = new User({name,email,password:hashedPassword});
         await user.save();
+
+        //create token and store cookie
+
+        res.clearCookie(COOKIE_NAME,{
+
+            domain:"localhost",
+             httpOnly:true,
+             signed:true,
+             path:"/",
+        });
+    
+        const token = createToken(user._id.toString(),user.email,"7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7)
+        res.cookie(COOKIE_NAME,token,{path:"/",domain:"localhost",expires, httpOnly:true,signed:true,})
+
         return res.status(201).json({message: "Ok", id: user._id.toString()});
     } catch (error) {
         
@@ -49,8 +66,18 @@ export const userLogin = async (req:Request,res:Response,next:NextFunction) => {
     return res.status(403).send("Incorrect Password");
     }
 
+    res.clearCookie(COOKIE_NAME,{
+
+        domain:"localhost",
+         httpOnly:true,
+         signed:true,
+         path:"/",
+    });
+
     const token = createToken(user._id.toString(),user.email,"7d");
-    res.cookie('auth_token',token)
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7)
+    res.cookie(COOKIE_NAME,token,{path:"/",domain:"localhost",expires, httpOnly:true,signed:true,})
 
 
         return res.status(200).json({message: "Ok", id: user._id.toString()});
