c3dbd361a56c2be6157a1ccfc5a71b7636cff522
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-02-04 23:39:42 +0500

    frontend working mern gpt

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { AppBar, Toolbar } from "@mui/material"
+import Logo from "./shared/Logo"
 
 const Header = () => {
   return (
     <>
       <AppBar sx={{bgcolor:'transparent',position:'static',boxShadow:"none"}}>
-        <Toolbar sx={{display:'flex'}}></Toolbar>
+        <Toolbar sx={{display:'flex'}}>
+          <Logo/>
+        </Toolbar>
       </AppBar>
     </>
   )
