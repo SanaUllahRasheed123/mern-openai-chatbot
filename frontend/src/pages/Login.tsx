f96698b37a5c1497a2a0fd60804fc246a25b0fac
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-02-06 22:47:19 +0500

    frontend working mern gpt..

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,16 @@
-import { Box } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
+import CustomizationInput from "../components/shared/CustomizationInput"
+import { IoIosLogIn } from "react-icons/io"
 
 const Login = () => {
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=> {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget)
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log(email,password);
+    
+  }
   return (
     <>
     <Box width={'100%'} height={'100%'} display='flex' flex={1}>
@@ -8,7 +18,62 @@ const Login = () => {
         <img src="airobot.png" alt="Robot" style={{width:"400px"}}/>
 
       </Box>
-      <Box></Box>
+      <Box
+       display={'flex'}
+       flex={{xs:1,md:0.5}} 
+       justifyContent={'center'}
+       alignItems={'center'}
+       padding={2}
+       ml={'auto'}
+       mt={16}
+       >
+        <form
+        onSubmit={(handleSubmit)}
+        style={{
+          margin: 'auto',
+          padding: '30px',
+          boxShadow:'10px 10px 20px #000',
+          borderRadius:'10px',
+          border:'none',
+        }}
+        >
+          <Box
+           sx={{
+            display:'flex',
+            flexDirection:'column',
+            justifyContent:'center'
+            }}
+            >
+              <Typography
+              variant="h4"
+              textAlign='center'
+              padding={2}
+              fontWeight={600}
+
+              
+              >Login
+              </Typography>
+              <CustomizationInput type="email" name="email" label="Email"/>
+              <CustomizationInput type="password" name="password" label="Password"/>
+              <Button
+              type="submit"
+              sx={{
+                px:2,
+                py:1,
+                mt:2,
+                width:'400px',
+                borderRadius:2,
+                bgcolor:'#00fffc',
+                ":hover":{
+                  bgcolor:'white',
+                  color:'black'
+                }
+              }}
+              endIcon={<IoIosLogIn/>}
+              >Login</Button>
+            </Box>
+        </form>
+      </Box>
 
     </Box>
     </>
