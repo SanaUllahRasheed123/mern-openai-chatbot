b18b902f5e865b1d618211918d6c417b4d1332c1
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-02-06 16:25:45 +0500

    frontend working mern gpt..

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,17 @@
+import { Box } from "@mui/material"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <Box width={'100%'} height={'100%'} display='flex' flex={1}>
+      <Box padding={8} mt={8} display={{md:'flex',sm:'none',xs:'none'}}>
+        <img src="airobot.png" alt="Robot" style={{width:"400px"}}/>
+
+      </Box>
+      <Box></Box>
+
+    </Box>
+    </>
   )
 }
 
