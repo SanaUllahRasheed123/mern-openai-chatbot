f3a071305a7ccf7ca811a1ec8c5a37cbbf0adeac
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-02-07 16:14:27 +0500

    35

--- backend/dist/utils/token-manager.js
+++ backend/dist/utils/token-manager.js
@@ -1,9 +1,31 @@
 import Jwt from "jsonwebtoken";
+import { COOKIE_NAME } from "./constants.js";
+// import { resolve } from "path";
 export const createToken = (id, email, expiresIn) => {
     const payload = { id, email };
     const token = Jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
     return token;
+};
+export const verifyToken = async (req, res, next) => {
+    const token = req.signedCookies[`${COOKIE_NAME}`];
+    if (!token || token.trim() === "") {
+        return res.status(401).json({ message: "Token Not Received" });
+    }
+    return new Promise((resolve, reject) => {
+        return Jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+            if (err) {
+                reject(err.message);
+                return res.status(401).json({ message: "Token Expired" });
+            }
+            else {
+                console.log("Token Verification Successfull");
+                resolve();
+                res.locals.jwtData = success;
+                return next();
+            }
+        });
+    });
 };
 //# sourceMappingURL=token-manager.js.map
